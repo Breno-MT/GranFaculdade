783c4b0de71b8bb02ea77c7417c435140fbd14cb
Breno-MT <brenomtdev@gmail.com>
2026-08-06 17:24:27 -0300

    FEAT: Conversão de Moedas - Origem, Destino e Valor das Moedas (Versão 2.0)

--- Projetos/ConversaoDeMoedas/app.py
+++ Projetos/ConversaoDeMoedas/app.py
@@ -1,17 +1,19 @@
 import requests
 
-def get_cotacao():
-    url = 'https://api.exchangerate-api.com/v4/latest/BRL'
+def get_cotacao(origem):
+    url = f'https://api.exchangerate-api.com/v4/latest/{origem}'
 
     response = requests.get(url)
 
     data = response.json()
 
     if response.status_code == 200:
-        print(f"BRL ao USD: US$:{1 / data['rates']['USD']:.2f}")
-        print(f"BRL ao EUR: EUR€:{1 / data['rates']['EUR']:.2f}")
+        return data["rates"]
     else:
         print('Erro ao buscar cotações: ', response.status_code)
-    
 
-get_cotacao()
+def converter_cotacao(origem = 'USD', destino = 'BRL', valor = 1):
+    rates = get_cotacao(destino)
+    return round(valor / rates[origem], 2)
+
+print(converter_cotacao())
