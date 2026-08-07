eb0aa134ccc9d7af74c6f22599daca60ef06cf84
Breno-MT <brenomtdev@gmail.com>
2026-08-07 09:47:20 -0300

    FEAT: Conversão de Moedas

--- Projetos/ConversaoDeMoedas/app.py
+++ Projetos/ConversaoDeMoedas/app.py
@@ -1,20 +1,4 @@
-import requests
-
-def get_cotacao(origem):
-    url = f'https://api.exchangerate-api.com/v4/latest/{origem}'
-
-    response = requests.get(url)
-
-    data = response.json()
-
-    if response.status_code == 200:
-        return data["rates"]
-    else:
-        print('Erro ao buscar cotações: ', response.status_code)
-
-def converter_cotacao(origem = 'USD', destino = 'BRL', valor = 1):
-    rates = get_cotacao(destino)
-    return round(valor / rates[origem], 2)
+from moedas import converter_cotacao
 
 def menu():
     print()
