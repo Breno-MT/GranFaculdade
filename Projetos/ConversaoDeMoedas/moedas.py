caf0ce396b81ff89fc4700d7b9f40a460f144d99
Breno-MT <brenomtdev@gmail.com>
2026-08-07 09:47:37 -0300

    FEAT: Funções para pegar cotações

---
+++ Projetos/ConversaoDeMoedas/moedas.py
@@ -0,0 +1,17 @@
+import requests
+
+def get_cotacao(origem = 'BRL'):
+    url = f'https://api.exchangerate-api.com/v4/latest/{origem}'
+
+    response = requests.get(url)
+
+    data = response.json()
+
+    if response.status_code == 200:
+        return data["rates"]
+    else:
+        print('Erro ao buscar cotações: ', response.status_code)
+
+def converter_cotacao(origem = 'USD', destino = 'BRL', valor = 1):
+    rates = get_cotacao(destino)
+    return round(valor / rates[origem], 2)
